a24b31ee04105e5db579faf0cce496f90ff6cca0
gavazcal <2392@holbertonschool.com>
2021-04-27 16:25:53 +0000

    print statement syntax

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -4,8 +4,10 @@ number = random.randint(-10000, 10000)
 last_digit = number % 10
 if number < 0:
     last_digit = (abs(number) % 10) * -1
-print("Last digit of {}".format(number))
-print("is {}".format(last_digit))
+print("Last digit of", end = " ")
+print("{}".format(number), end = " ")
+print("is", end = " ")
+print("{}".format(last_digit), end = " ")
 if last_digit > 5:
     print("and is greater than 5")
 elif last_digit == 0:
