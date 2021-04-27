3784a880c089f5125686cbc77e5c2b2a8335bbdf
gavazcal <2392@holbertonschool.com>
2021-04-27 17:16:13 +0000

    end format

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 import string
 for i in range(97, 101):
-    print(chr(i), end="")
+    print("{}".format(chr(i)), end="")
 for i in range (102, 113):
-    print(chr(i), end="")
+    print("{}".format(chr(i)), end="")
 for i in range (114, 123):
-    print(chr(i), end="")
+    print("{}".format(chr(i)), end="")
