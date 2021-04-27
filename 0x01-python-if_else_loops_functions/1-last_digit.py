1304de804ec22bead6d26f7bfc489457dfcfffec
gavazcal <2392@holbertonschool.com>
2021-04-27 16:43:54 +0000

    removing duplicate string

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -5,10 +5,10 @@ last_digit = number % 10
 if number < 0:
     last_digit = (abs(number) % 10) * -1
 if last_digit > 5:
-    string = "and is greater than 5"
+    string = "greater than 5"
 elif last_digit == 0:
-    string = "and is 0"
-elif last_digit < 5:
-    string = "and is less than 6 and not 0"
+    string = "0"
+else:
+    string = "less than 6 and not 0"
 
 print("Last digit of {:d} is {:d} and is {:s}".format(number, last_digit, string))
