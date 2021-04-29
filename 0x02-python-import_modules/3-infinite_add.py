59377ff34c99d779880067c06f3e34ec010022aa
gavazcal <2392@holbertonschool.com>
2021-04-29 18:12:59 +0000

    Adding task 3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+    result = 0
+    if len(argv) == 0:
+        print("0")
+    elif len(argv) > 0:
+        for x in range(1, len(argv)):
+            result += int(argv[x])
+            print("{:d}".format(result))
