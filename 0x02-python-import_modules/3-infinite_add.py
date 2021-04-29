035f5c378f120f25f518ba767d51c315e9ca4792
gavazcal <2392@holbertonschool.com>
2021-04-29 18:14:30 +0000

    removed extra loop

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -7,4 +7,4 @@ if __name__ == "__main__":
     elif len(argv) > 0:
         for x in range(1, len(argv)):
             result += int(argv[x])
-            print("{:d}".format(result))
+        print("{:d}".format(result))
