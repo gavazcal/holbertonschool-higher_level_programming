219a342bff641d401a806dde42526513861ca8b3
gavazcal <2392@holbertonschool.com>
2021-05-06 20:52:35 +0000

    Testing

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,6 +3,9 @@
 
 def best_score(a_dictionary):
     if (a_dictionary):
-        return (max(a_dictionary.values()))
+        largest = max(a_dictionary.values())
+        for key, val in a_dictionary.items():
+            if val is largest:
+                return key
     else:
         return None
