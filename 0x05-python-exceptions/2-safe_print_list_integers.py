60959e06134240958eadeec1a05753dfa50c8f26
gavazcal <2392@holbertonschool.com>
2021-05-18 01:22:30 +0000

    fixed range

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -6,7 +6,7 @@ def safe_print_list_integers(my_list=[], x=0):
 
     for y in range(x):
         try:
-            print("{:d}".format(my_list[nums]), end="")
+            print("{:d}".format(my_list[y]), end="")
             nums += 1
         except (ValueError, TypeError):
             continue
