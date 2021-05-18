99922377fd093718763741c329ccef3a7584b026
gavazcal <2392@holbertonschool.com>
2021-05-18 00:56:44 +0000

    task 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+
+def safe_print_list_integers(my_list=[], x=0):
+    nums = 0
+
+    for y in range(x):
+        try:
+            print("{:d}".format(my_list[nums]), end="")
+            nums += 1
+        except (ValueError, TypeError):
+            continue
+    print()
+    return nums
