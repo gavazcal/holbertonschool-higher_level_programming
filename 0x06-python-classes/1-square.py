ddaaa7b7b8f4727c39a5119e8ac93c25c620d56a
gavazcal <2392@holbertonschool.com>
2021-05-18 21:04:15 +0000

    task 1:

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""defines a square class"""
+
+
+class Square:
+    """square class"""
+    def __init__(self, size):
+        """adds size instance"""
+        self.__size = size
