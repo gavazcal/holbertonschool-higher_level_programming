b5b16a74a9f81b2a5c24c8896537f910aced5da4
gavazcal <2392@holbertonschool.com>
2021-05-18 01:42:15 +0000

    advanced task 100

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+
+def safe_print_integer_err(value):
+    import sys
+    try:
+        print("{:d}".format(value))
+        return True
+    except Exception as failure:
+        stderr.write("Exception: {}", format(failure))
+        return False
