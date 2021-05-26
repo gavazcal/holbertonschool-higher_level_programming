7b35c3620f60310daca4ae40a8fdecda028d8046
gavazcal <2392@holbertonschool.com>
2021-05-26 15:17:50 +0000

    why

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-'''This module contains is a single method'''
+"""prints <first name> <last name>"""
 
 
 def say_my_name(first_name, last_name=""):
-    '''Prints a message'''
+    """prints first and last name"""
 
     if type(first_name) is not str:
         raise TypeError("first_name must be a string")
+
     if type(last_name) is not str:
         raise TypeError("last_name must be a string")
-
-    print("My name is {} {}".format(first_name, last_name))
+    print("My name is {:s} {:s}".format(first_name, last_name))
