401497dfad0b78ac1229f904ba57c36e9511aee8
gavazcal <2392@holbertonschool.com>
2021-06-02 20:24:37 +0000

    task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""boolean function to check instance and object class"""
+
+
+def is_kind_of_class(obj, a_class):
+    """boolean function"""
+    return isinstance(obj, a_class)
