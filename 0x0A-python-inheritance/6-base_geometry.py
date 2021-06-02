94766b5414fda8143e61cb28dbec7d2fe41bf32e
gavazcal <2392@holbertonschool.com>
2021-06-02 23:05:01 +0000

    task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""creates a BaseGeometry class"""
+
+
+class BaseGeometry:
+    """initializes class with attributes"""
+
+    def area(self):
+        """area handling"""
+        raise Exception("area() is not implemented")
