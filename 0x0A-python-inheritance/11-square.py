dc3032221021def25fa4d51426d321f797ee7390
gavazcal <2392@holbertonschool.com>
2021-06-03 00:40:31 +0000

    task 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""defines a square class"""
+Rectangle = __import__('9-rectangle').Rectangle
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Square(Rectangle):
+    """inherits from rectangle"""
+
+    def __init__(self, size):
+        """size instantiation"""
+        BaseGeometry.integer_validator(self, "size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        """string"""
+
+        return ("[Square] {:d}/{:d}".format(self.__size, self.__size))
