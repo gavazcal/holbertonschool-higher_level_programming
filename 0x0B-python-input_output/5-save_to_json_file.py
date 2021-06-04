25e6568161b71758eac2c97c1373442b453e9287
gavazcal <2392@holbertonschool.com>
2021-06-04 02:31:03 +0000

    Task 5

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""json save"""
+
+
+def save_to_json_file(my_obj, filename):
+    """json save"""
+    import json
+    with open(filename, 'w') as a_file:
+        json.dump(my_obj, a_file
