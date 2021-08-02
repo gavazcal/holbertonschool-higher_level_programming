f2d10c46f25bf6102ff75551f3763554cf17aa10
gavazcal <2392@holbertonschool.com>
2021-08-02 23:42:19 +0000

    task 6 semistandard compliant

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/nodejs
+// prints 3 lines with one print and a loop
+
+const strings = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let idx = 0; idx < strings.length; idx++) {
+  console.log(strings[idx]);
+}
