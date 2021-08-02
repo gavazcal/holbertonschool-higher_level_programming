a4ef771755c0f961fa15eb16811f1edbd5841a4e
gavazcal <2392@holbertonschool.com>
2021-08-02 22:04:16 +0000

    task 1, semistandard compliant

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/nodejs
+// prints out 3 statements
+
+const strArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (let idx = 0; idx < strArray.length; idx++) {
+  console.log(strArray[idx]);
+}
