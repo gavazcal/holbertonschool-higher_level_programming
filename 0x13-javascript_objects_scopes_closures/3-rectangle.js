869a709944cb0e5d47f85dbcd081b9faee1e1f72
gavazcal <2392@holbertonschool.com>
2021-08-04 01:14:17 +0000

    task 3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,18 @@
+#!/usr/bin/nodejs
+// Defines a rectangle class
+
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+    print () {
+    for (let idx = 0; idx < this.width; idx++) {
+      console.log('X'.repeat(this.height));
+    }
+  }
+}
+
+module.exports = Rectangle;
