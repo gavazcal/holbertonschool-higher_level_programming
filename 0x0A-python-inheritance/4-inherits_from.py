c7d32989900f7f9c2524e56f67f0175bbdd66c1a
gavazcal <2392@holbertonschool.com>
2021-08-13 18:53:45 +0000

    merging

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,4 +4,7 @@
 
 def inherits_from(obj, a_class):
     """boolean func"""
-    return issubclass(type(obj), a_class)
+    if issubclass(type(obj), a_class):
+        return True
+    else:
+        return False
