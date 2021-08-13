643a25cef5a838fd477c6a631c74b1653275ae39
gavazcal <2392@holbertonschool.com>
2021-08-13 17:19:17 +0000

    filename typo

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" lists all states with name starting in N """
+
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=sys.argv[1], passwd=sys.argv[2],
+                         db=sys.argv[3])
+
+    cursor = db.cursor()
+
+    cursor.execute("SELECT * FROM states\
+                    WHERE BINARY name LIKE '{}'\
+                    ORDER BY id ASC".format(sys.argv[4]))
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+
+    cursor.close()
+    db.close()
