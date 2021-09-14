230f16db246d298a083f477981781a1dbc8b7cf9
gavazcal <2392@holbertonschool.com>
2021-09-14 23:00:45 +0000

    task 1, fixed console log when err

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,7 +5,6 @@ const fs = require('fs');
 const file = process.argv[2];
 const string = process.argv[3];
 
-fs.writeFile(file, string, 'utf8', function (err) {
+fs.writeFile(file, string, 'utf8', (err) => {
   if (err) return console.log(err);
-  console.log('string > file');
 });
