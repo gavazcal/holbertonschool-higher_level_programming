cb764f357277f4f574a908025b4fe7ff47c962ca
gavazcal <2392@holbertonschool.com>
2021-09-15 01:40:19 +0000

    made semistandard compliant

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -7,17 +7,17 @@ const path = URL + '/films';
 const movienumber = process.argv[2] - 1;
 
 request(path, (error, response, body) => {
-	if (error || movienumber < 0) return console.log(error);
+  if (error || movienumber < 0) return console.log(error);
 
-	const allfilmdata = JSON.parse(body);
-	const specificfilmdata = allfilmdata.results[movienumber];
-	const cast = specificfilmdata.characters;
+  const allfilmdata = JSON.parse(body);
+  const specificfilmdata = allfilmdata.results[movienumber];
+  const cast = specificfilmdata.characters;
 
-	for (const character of cast) {
-		request(character, (error, response, body) => {
-			if (error) return console.log(error);
-			const filmcast = JSON.parse(body);
-			console.log(filmcast.name);
-		});
-	}
+  for (const character of cast) {
+    request(character, (error, response, body) => {
+      if (error) return console.log(error);
+      const filmcast = JSON.parse(body);
+      console.log(filmcast.name);
+    });
+  }
 });
