955a9da53212b4a366e7d9769e021be9dadc6226
gavazcal <2392@holbertonschool.com>
2021-09-15 02:16:06 +0000

    changed the name of a const which was interfering with the completed class

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,5 +1,5 @@
 #!/usr/bin/nodejs
-// returns number of tasks done by user id
+// returns number of tasks completed by user id
 
 const request = require('request');
 const URL = process.argv[2];
@@ -7,14 +7,14 @@ const URL = process.argv[2];
 request(URL, (error, response, body) => {
   if (error) return console.log(error);
   const data = JSON.parse(body);
-  const done = {};
+  const completed = {};
   let usercount;
 
   for (const count of data) {
     usercount = count.userId;
-    if (count.done === true) {
-      done[usercount] = (done[usercount] || 0) + 1;
+    if (count.completed === true) {
+      completed[usercount] = (completed[usercount] || 0) + 1;
     }
   }
-  console.log(done);
+  console.log(completed);
 });
