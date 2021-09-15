265d0e09828712de97dcbd80c3f45a3d114940e5
gavazcal <2392@holbertonschool.com>
2021-09-15 01:39:20 +0000

    semistandard compliant, correct output, task 6

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,23 @@
+#!/usr/bin/nodejs
+// prints out star wars characters
+
+const request = require('request');
+const URL = 'https://swapi-api.hbtn.io/api';
+const path = URL + '/films';
+const movienumber = process.argv[2] - 1;
+
+request(path, (error, response, body) => {
+	if (error || movienumber < 0) return console.log(error);
+
+	const allfilmdata = JSON.parse(body);
+	const specificfilmdata = allfilmdata.results[movienumber];
+	const cast = specificfilmdata.characters;
+
+	for (const character of cast) {
+		request(character, (error, response, body) => {
+			if (error) return console.log(error);
+			const filmcast = JSON.parse(body);
+			console.log(filmcast.name);
+		});
+	}
+});
