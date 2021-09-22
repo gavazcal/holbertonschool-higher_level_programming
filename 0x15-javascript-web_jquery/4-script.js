6bb3ce7c49c2e23315979a1ccf8581e2d44e0ae7
gavazcal <2392@holbertonschool.com>
2021-09-22 21:27:23 +0000

    fixed missing code:

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -3,6 +3,8 @@
 $('DIV#toggle_header').click(function () {
 	const $h = $('header');
 	if ($h.hasClass('green')) {
+		$h.removeClass('green').addClass('red');
+	} else {
 		$h.removeClass('red').addClass('green');
 	}
 });
