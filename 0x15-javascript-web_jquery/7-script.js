1ff51e69befabd6ccd82be39180b1d8a86ab37a5
gavazcal <2392@holbertonschool.com>
2021-09-22 20:53:29 +0000

    task 7

---
+++ 0x15-javascript-web_jquery/7-script.js
@@ -0,0 +1,11 @@
+#!/usr/bin/nodejs
+
+$(function () {
+	$.ajax({
+		type: 'GET',
+		url: 'https://swapi-api.hbtn.io/api/people/5/?format=json',
+		success: function (people) {
+			$('div#character').text(people.name)
+		}
+	});
+});
