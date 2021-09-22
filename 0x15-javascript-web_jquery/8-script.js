4b16e5bbb9eddd071661fcc0a2ccd1bcbcfc6f4c
gavazcal <2392@holbertonschool.com>
2021-09-22 20:56:40 +0000

    task 8

---
+++ 0x15-javascript-web_jquery/8-script.js
@@ -0,0 +1,13 @@
+#!/usr/bin/nodejs
+
+$(function () {
+	$.ajax({
+		type: 'GET',
+		url: 'https://swapi-api.hbtn.io/api/films/?format=json',
+		success: function (films) {
+			$.each(films.results, function (i, movie) {
+				$('ul#list_movies').append('<li>' + movie.title + '</li>');
+			});
+		}
+	});
+});
